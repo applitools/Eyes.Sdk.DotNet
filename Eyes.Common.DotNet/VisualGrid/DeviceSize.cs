bf675e5e1d16dfd2662a1d9316261fcb26afa804
Itai Bar-Haim <itai.barhaim@applitools.com>
2020-09-29 14:54:08 +0300

    fixed a serialization bug

--- Eyes.Common.DotNet/VisualGrid/DeviceSize.cs
+++ Eyes.Common.DotNet/VisualGrid/DeviceSize.cs
@@ -1,20 +1,20 @@
-﻿using Newtonsoft.Json;
-using System.Drawing;
+﻿using Applitools.Utils.Geometry;
+using Newtonsoft.Json;
 
 namespace Applitools.VisualGrid.Model
 {
     public class DeviceSize
     {
         [JsonProperty("portrait")]
-        public Size? Portrait { get; set; }
+        public RectangleSize Portrait { get; set; }
 
         [JsonProperty("landscapeLeft")]
-        public Size? LandscapeLeft { get; set; }
+        public RectangleSize LandscapeLeft { get; set; }
         
         [JsonProperty("landscapeRight")]
-        public Size? LandscapeRight { get; set; }
+        public RectangleSize LandscapeRight { get; set; }
 
         [JsonProperty("landscape")]
-        public Size? Landscape { get; set; }
+        public RectangleSize Landscape { get; set; }
     }
 }
