d42dedb022982aa13eeb3fd15c3d28ff0759494a
Itai Bar-Haim <itai.barhaim@applitools.com>
2020-10-25 13:29:54 +0200

    Added TestRunnerConcurrency

--- Tests/Test.Eyes.Selenium.DotNet/VisualGridTests/TestVisualGridApi.cs
+++ Tests/Test.Eyes.Selenium.DotNet/VisualGridTests/TestVisualGridApi.cs
@@ -9,6 +9,8 @@ using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using ScreenOrientation = Applitools.VisualGrid.ScreenOrientation;
 
 namespace Applitools.Selenium.Tests.VisualGridTests
 {
@@ -151,6 +153,7 @@ namespace Applitools.Selenium.Tests.VisualGridTests
             eyes.SetConfiguration(config);
 
             MockEyesConnector mockEyesConnector;
+            object errorMessageLocker = new object();
             string errorMessage = null;
 
             IWebDriver driver = SeleniumUtils.CreateChromeDriver();
@@ -163,7 +166,10 @@ namespace Applitools.Selenium.Tests.VisualGridTests
                     IVisualGridEyes eyes = runner.allEyes_.First();
                     if (!eyes.GetAllRunningTests().First().IsTestOpen)
                     {
-                        errorMessage = "Render called before open";
+                        lock (errorMessageLocker)
+                        {
+                            errorMessage = "Render called before open";
+                        }
                     }
 
                     return (true, null);
@@ -179,6 +185,67 @@ namespace Applitools.Selenium.Tests.VisualGridTests
             }
 
             Assert.IsNull(errorMessage);
+        }
+
+        [Test]
+        public void TestRunnerConcurrency()
+        {
+            VisualGridRunner runner = new VisualGridRunner(new RunnerOptions().TestConcurrency(3));
+            Eyes eyes = new Eyes(runner);
+            TestUtils.SetupLogging(eyes);
+            eyes.visualGridEyes_.EyesConnectorFactory = new MockEyesConnectorFactory();
+
+            Configuration config = eyes.GetConfiguration();
+            config.AddBrowser(new IosDeviceInfo(IosDeviceName.iPhone_7));
+            config.AddDeviceEmulation(DeviceName.Galaxy_S5, ScreenOrientation.Landscape);
+            config.AddBrowser(new DesktopBrowserInfo(800, 800, BrowserType.CHROME));
+            config.AddBrowser(new DesktopBrowserInfo(800, 800, BrowserType.FIREFOX));
+            config.AddBrowser(new DesktopBrowserInfo(800, 800, BrowserType.SAFARI));
+            eyes.SetConfiguration(config);
+
+            MockEyesConnector mockEyesConnector;
+
+            int currentlyOpenTests = 0;
+            bool isFail = false;
+
+            IWebDriver driver = SeleniumUtils.CreateChromeDriver();
+            driver.Url = "http://applitools.github.io/demo";
+            try
+            {
+                mockEyesConnector = OpenEyesAndGetConnector_(eyes, config, driver);
+                EyesConnector eyesConnector = (EyesConnector)mockEyesConnector.WrappedConnector;
+                MockServerConnector mockServerConnector = (MockServerConnector)eyesConnector.ServerConnector;
+
+                mockServerConnector.OnStartSession += (startSessionInfo) =>
+                {
+                    if (Interlocked.Increment(ref currentlyOpenTests) > 3)
+                    {
+                        isFail = true;
+                    }
+                    Thread.Sleep(3000);
+                    if (currentlyOpenTests > 3)
+                    {
+                        isFail = true;
+                    }
+                    return (true, null);
+                };
+
+                mockServerConnector.AfterEndSession += (runningSession, isAborted, save) =>
+                {
+                    Interlocked.Decrement(ref currentlyOpenTests);
+                };
+
+                eyes.Check(Target.Window().Fully());
+                eyes.CloseAsync();
+            }
+            finally
+            {
+                eyes.AbortAsync();
+                driver.Quit();
+                runner.GetAllTestResults(false);
+            }
+
+            Assert.IsFalse(isFail, "Number of open tests was higher than the concurrency limit");
         }
 
         private static MockEyesConnector OpenEyesAndGetConnector_(Eyes eyes, Configuration config, IWebDriver driver)
