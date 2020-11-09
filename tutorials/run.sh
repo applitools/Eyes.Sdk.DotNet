bc6ce0f4a1128d155d2d93022212ff51388dcb63
artem <artem.borodavka@applitools.com>
2020-11-09 12:23:13 +0200

    Update run script to lower console output

--- tutorials/run.sh
+++ tutorials/run.sh
@@ -3,6 +3,8 @@
 set -e
 DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 $DIR/build.sh
+echo "Run the basic tutorial"
 docker run -e APPLITOOLS_API_KEY dotnet_basic 
+echo "Run the ufg tutorial"
 docker run -e APPLITOOLS_API_KEY dotnet_ufg 
 $DIR/report.sh
