92bf022a3728080d372032a4c27144162082c979
Itai Bar-Haim <itai.barhaim@applitools.com>
2020-11-12 16:27:31 +0200

    wip - added test

---
+++ Tests/Test.Eyes.Sdk.Core.DotNet/TestBatchClose.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Applitools.Tests
+{
+    [Parallelizable(ParallelScope.All)]
+    public class TestBatchCloseClass
+    {
+        private class MockServerConnector : ServerConnector
+        {
+            public MockServerConnector() : base(new Logger())
+            {
+            }
+        }
+
+        [Test]
+        public void TestBatchClose()
+        {
+            List<string> batchIds = new List<string>();
+            batchIds.Add("first");
+            batchIds.Add("second");
+            batchIds.Add("third");
+
+            Uri serverUrl1 = new Uri("http://customUrl1");
+            Uri serverUrl2 = new Uri("http://customUrl2");
+
+            BatchClose batchClose = new BatchClose();
+            EnabledBatchClose enabledBatchClose = batchClose.SetUrl(serverUrl1).SetBatchId(batchIds);
+            Assert.AreEqual(serverUrl1, enabledBatchClose.ServerUrl);
+            enabledBatchClose.SetUrl(serverUrl2);
+            Assert.AreEqual(serverUrl2, enabledBatchClose.ServerUrl);
+
+            ServerConnector serverConnector = new MockServerConnector();
+            enabledBatchClose.serverConnector_ = serverConnector;
+
+            enabledBatchClose.Close();
+            serverConnector.CloseBatch("first", true, serverUrl2);
+            serverConnector.CloseBatch("second", true, serverUrl2);
+            serverConnector.CloseBatch("third", true, serverUrl2);
+        }
+    }
+}
