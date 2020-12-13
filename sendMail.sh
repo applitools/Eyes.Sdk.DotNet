1589d166d4f2625c31be4ed984420f73b1b9b5ff
Itai Bar-Haim <itai.barhaim@applitools.com>
2020-12-13 13:23:31 +0200

    changed conditions.

--- sendMail.sh
+++ sendMail.sh
@@ -2,7 +2,7 @@
 COMMITTER_EMAIL=$(git log -1 $TRAVIS_COMMIT --pretty="%cE")
 echo "$COMMITTER_EMAIL"
 
-if [[ $TRAVIS_EVENT_TYPE = cron ]]; then
+if [[ $APPLITOOLS_FULL_COVERAGE = true ]]; then
 	curl http://sdk-test-results.herokuapp.com/send_full_regression/sdks -X POST -H "Content-Type: application/json" -d @SEND_MAIL.json
 else
 	curl http://sdk-test-results.herokuapp.com/send_mail/sdks -X POST -H "Content-Type: application/json" -d @SEND_MAIL.json
