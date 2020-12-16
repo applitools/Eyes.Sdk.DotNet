1fbcac1a26b58f90500200fccb675d9237dcdb2b
Itai Bar-Haim <itai.barhaim@applitools.com>
2020-12-16 09:28:17 +0200

    fixing full coverage cron job

--- sendMail.sh
+++ sendMail.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 COMMITTER_EMAIL=$(git log -1 $TRAVIS_COMMIT --pretty="%cE")
 echo "$COMMITTER_EMAIL"
-
-if [[ $APPLITOOLS_FULL_COVERAGE = true ]]; then
-	curl http://sdk-test-results.herokuapp.com/send_full_regression/sdks -X POST -H "Content-Type: application/json" -d @SEND_MAIL.json
-else
-	curl http://sdk-test-results.herokuapp.com/send_mail/sdks -X POST -H "Content-Type: application/json" -d @SEND_MAIL.json
-fi
+curl http://sdk-test-results.herokuapp.com/send_mail/sdks -X POST -H "Content-Type: application/json" -d @SEND_MAIL.json
