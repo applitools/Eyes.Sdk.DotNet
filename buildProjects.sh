a36a4d8ebdb369e867fea3dabd84fe893404cc7c
Itai Bar-Haim <itai.barhaim@applitools.com>
2020-12-30 08:59:41 +0200

    an attempt to fix build time.

--- buildProjects.sh
+++ buildProjects.sh
@@ -11,10 +11,10 @@ cd ..
 
 echo "running tests"
 
-dotnet test -f netcoreapp3.1 Eyes.Sdk.DotNet_Travis.sln
+dotnet test -f netcoreapp3.1 Eyes.Sdk.DotNet_Travis.sln --filter $1
 result=$?
 echo $result
 if [ $result -ne 0 ]; then
     echo "Not all tests passed... Retrying."
-    dotnet test -f netcoreapp3.1 Eyes.Sdk.DotNet_Travis.sln
+    dotnet test -f netcoreapp3.1 Eyes.Sdk.DotNet_Travis.sln --filter $1
 fi
