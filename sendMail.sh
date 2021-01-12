5396ce324ef154f2f880fa5afe283c77fa633257
Itai Bar-Haim <itai.barhaim@applitools.com>
2021-01-12 11:48:30 +0200

    fixed sendMail.sh

--- sendMail.sh
+++ sendMail.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 COMMITTER_EMAIL=$(git log -1 $TRAVIS_COMMIT --pretty="%cE")
 echo "$COMMITTER_EMAIL"
-curl http://sdk-test-results.herokuapp.com/send_mail/sdks -X POST -H "Content-Type: application/json" -d @SEND_MAIL.json
+curl http://sdk-test-results.herokuapp.com/send_mail -X POST -H "Content-Type: application/json" -d @SEND_MAIL.json
