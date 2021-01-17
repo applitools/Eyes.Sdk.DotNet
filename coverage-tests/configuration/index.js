18fdea1dd3fce1619231c8b4304a6ccf6d36e4f4
YuriyCheTest <yurii.cherepovskiy@applitools.com>
2021-01-18 00:52:22 +0200

    Update links to dependencies

--- coverage-tests/configuration/index.js
+++ coverage-tests/configuration/index.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'eyes_selenium_dotnet',
   emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/moveDotNetToMaster/DotNet/emitter.js',
   overrides: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/moveDotNetToMaster/DotNet/overrides.js',
-  template: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/moveDotNetToMaster/template.hbs',
+  template: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/moveDotNetToMaster/DotNet/template.hbs',
   ext: '.cs',
   outPath: './test/Selenium/coverage/generic'
 }
