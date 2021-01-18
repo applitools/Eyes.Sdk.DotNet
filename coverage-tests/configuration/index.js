b6f4d7afa6ca653bd62e3251714599ecd1dbbcba
YuriyCheTest <yurii.cherepovskiy@applitools.com>
2021-01-18 20:26:58 +0200

    Set link to master branch instead from test branch

--- coverage-tests/configuration/index.js
+++ coverage-tests/configuration/index.js
@@ -1,8 +1,8 @@
 module.exports = {
   name: 'eyes_selenium_dotnet',
-  emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/moveDotNetToMaster/DotNet/emitter.js',
-  overrides: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/moveDotNetToMaster/DotNet/overrides.js',
-  template: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/moveDotNetToMaster/DotNet/template.hbs',
+  emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/DotNet/emitter.js',
+  overrides: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/DotNet/overrides.js',
+  template: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/DotNet/template.hbs',
   ext: '.cs',
   outPath: './test/Selenium/coverage/generic'
 }
