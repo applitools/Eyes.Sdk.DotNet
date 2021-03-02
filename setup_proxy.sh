7f729fc996ee425338dac8f5c7655de20804798d
Itai Bar-Haim <itai.barhaim@applitools.com>
2021-03-02 15:26:53 +0200

    log network traffic on travis

---
+++ setup_proxy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+openssl req -new -newkey rsa:4096 -days 1 -nodes -x509 -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=*.applitools.com" -keyout DO_NOT_TRUST.key  -out DO_NOT_TRUST.crt
+cat DO_NOT_TRUST.key DO_NOT_TRUST.crt > DO_NOT_TRUST.pem
+sudo mkdir /usr/share/ca-certificates/extra
+sudo cp DO_NOT_TRUST.crt /usr/share/ca-certificates/extra
+sudo update-ca-certificates
+sudo apt-get install mitmproxy
+sudo mitmdump --showhost --cert=cert.pem -k > "commit_${TRAVIS_COMMIT:0:7}_network.log"
+export APPLITOOLS_PROXY=http://127.0.0.1:8080
