6452a2c3cd656ed7f04c9e72b1b3e5e60f5b3d83
Itai Bar-Haim <itai.barhaim@applitools.com>
2021-03-03 12:00:11 +0200

    added upload_to_azure.sh script and use it.

---
+++ upload_to_azure.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DATE_NOW=$(date -Ru | sed 's/\+0000/GMT/')
+AZ_VERSION="2018-03-28"
+AZ_BLOB_URL="https://sdksstorage.blob.core.windows.net"
+AZ_BLOB_CONTAINER="itai"
+AZ_BLOB_TARGET="${AZ_BLOB_URL}/${AZ_BLOB_CONTAINER}/"
+
+curl -v -X PUT -H "Content-Type: application/octet-stream" -H "x-ms-date: ${DATE_NOW}" -H "x-ms-version: ${AZ_VERSION}" -H "x-ms-blob-type: BlockBlob" --data-binary "@network.log" "${AZ_BLOB_TARGET}${TRAVIS_JOB_NUMBER}.network.log?${AZ_SAS_TOKEN}"
