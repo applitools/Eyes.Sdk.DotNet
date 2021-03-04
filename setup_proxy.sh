eeea1ef8900074add7dfcdf1d0c5e797723653ee
Itai Bar-Haim <itai.barhaim@applitools.com>
2021-03-04 14:47:32 +0200

    yet another attempt to enable proxy

--- setup_proxy.sh
+++ setup_proxy.sh
@@ -5,5 +5,4 @@ sudo mkdir /usr/share/ca-certificates/extra
 sudo cp DO_NOT_TRUST.crt /usr/share/ca-certificates/extra
 sudo update-ca-certificates
 sudo apt-get install mitmproxy
-ls -la `locate har_dump.py`
-mitmdump --cert=DO_NOT_TRUST.pem -s `locate har_dump.py` --set hardump=./network.log.har &
+mitmdump --cert=DO_NOT_TRUST.pem -s ./har_dump.py --set hardump=./network.log.zhar &
