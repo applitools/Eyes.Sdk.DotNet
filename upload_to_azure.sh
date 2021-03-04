096cdca46f1ab13ae702a9f9f8c33e6b698de70c
Itai Bar-Haim <itai.barhaim@applitools.com>
2021-03-04 10:14:59 +0200

    maybe the upload_to_azure will work that way?

--- upload_to_azure.sh
+++ upload_to_azure.sh
@@ -5,5 +5,6 @@ AZ_VERSION="2018-03-28"
 AZ_BLOB_URL="https://sdksstorage.blob.core.windows.net"
 AZ_BLOB_CONTAINER="itai"
 AZ_BLOB_TARGET="${AZ_BLOB_URL}/${AZ_BLOB_CONTAINER}/"
+AZ_SAS_TOKEN=$1
 
 curl -v -X PUT -H "Content-Type: application/octet-stream" -H "x-ms-date: ${DATE_NOW}" -H "x-ms-version: ${AZ_VERSION}" -H "x-ms-blob-type: BlockBlob" --data-binary "@network.log.har" "${AZ_BLOB_TARGET}${TRAVIS_JOB_NUMBER}.har?${AZ_SAS_TOKEN}"
