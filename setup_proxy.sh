f3c7e8f32634ae44e31214989287cdac1ac8dde9
Itai Bar-Haim <itai.barhaim@applitools.com>
2021-03-04 16:14:21 +0200

    is there even python3 installed?

--- setup_proxy.sh
+++ setup_proxy.sh
@@ -7,4 +7,5 @@ sudo update-ca-certificates
 sudo apt-get install mitmproxy
 alias python=python3
 python --version
+python3 --version
 mitmdump --cert=DO_NOT_TRUST.pem -s ./har_dump.py --set hardump=./network.log.zhar &
