097a7beeea1bcc384e58d46afd258ac2499490c1
Itai Bar-Haim <itaibh@gmail.com>
2021-03-06 10:33:20 +0200

    wip

--- setup_proxy.sh
+++ setup_proxy.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-openssl req -new -newkey rsa:4096 -days 1 -nodes -x509 -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=*.applitools.com" -keyout DO_NOT_TRUST.key  -out DO_NOT_TRUST.crt
-cat DO_NOT_TRUST.key DO_NOT_TRUST.crt > DO_NOT_TRUST.pem
-sudo mkdir /usr/share/ca-certificates/extra
-sudo cp DO_NOT_TRUST.crt /usr/share/ca-certificates/extra
-sudo update-ca-certificates
 sudo apt-get install mitmproxy
-mitmdump --cert=DO_NOT_TRUST.pem -s ./har_dump.py --set hardump=./network.log.har &
+sudo mkdir /usr/share/ca-certificates/extra
+sudo cp ~/.mitmproxy/mitmproxy-ca-cert.cer /usr/share/ca-certificates/extra
+sudo echo "extra/mitmproxy-ca-cert.cer" >> /etc/ca-certificates.conf
+sudo update-ca-certificates
+mitmdump -s ./har_dump.py --set hardump=./network.log.har &
