2b4d8bff2075c93b6ed82dc725e3ad0375548d76
Itai Bar-Haim <itaibh@gmail.com>
2021-03-06 13:02:14 +0200

    wip

--- setup_proxy.sh
+++ setup_proxy.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 sudo apt-get install mitmproxy
 mitmdump &
-curl --proxy "http://127.0.0.1:8080" https://mitm.it
+sleep 1
+curl --proxy "http://127.0.0.1:8080" http://mitm.it/
+sleep 1
 pkill mitmdump
+ls -la ~
 sudo mkdir /usr/share/ca-certificates/extra
 sudo cp ~/.mitmproxy/mitmproxy-ca-cert.cer /usr/share/ca-certificates/extra
-sudo echo "extra/mitmproxy-ca-cert.cer" >> /etc/ca-certificates.conf
+echo "extra/mitmproxy-ca-cert.cer" | sudo tee -a /etc/ca-certificates.conf
 sudo update-ca-certificates
 mitmdump -s ./har_dump.py --set hardump=./network.log.har &
