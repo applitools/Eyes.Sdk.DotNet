7056752699f8a55186c6fa65aaa1dbb62d6bdcd1
Itai Bar-Haim <itaibh@gmail.com>
2021-03-06 14:30:42 +0200

    wip

--- setup_proxy.sh
+++ setup_proxy.sh
@@ -5,9 +5,8 @@ sleep 1
 curl --proxy "http://127.0.0.1:8080" http://mitm.it/
 sleep 1
 pkill mitmdump
-ls -la ~
 sudo mkdir /usr/share/ca-certificates/extra
 sudo cp ~/.mitmproxy/mitmproxy-ca-cert.cer /usr/share/ca-certificates/extra
 echo "extra/mitmproxy-ca-cert.cer" | sudo tee -a /etc/ca-certificates.conf
 sudo update-ca-certificates
-mitmdump -s ./har_dump.py --set hardump=./network.log.har &
+mitmdump -s ./har_dump.py --set hardump=./network.log.har > ./network.log &
--- upload_to_azure.sh
+++ upload_to_azure.sh
@@ -7,4 +7,5 @@ AZ_BLOB_CONTAINER="itai"
 AZ_BLOB_TARGET="${AZ_BLOB_URL}/${AZ_BLOB_CONTAINER}/"
 AZ_SAS_TOKEN="sp=rc&st=2021-03-04T08:56:00Z&se=2023-03-04T16:56:00Z&spr=https&sv=2020-02-10&sr=c&sig=$1"
 
-curl -v -X PUT -H "Content-Type: application/octet-stream" -H "x-ms-date: ${DATE_NOW}" -H "x-ms-version: ${AZ_VERSION}" -H "x-ms-blob-type: BlockBlob" --data-binary "@network.log.har.gz" "${AZ_BLOB_TARGET}${TRAVIS_JOB_NUMBER}.har?${AZ_SAS_TOKEN}"
+curl -v -X PUT -H "Content-Type: application/octet-stream" -H "x-ms-date: ${DATE_NOW}" -H "x-ms-version: ${AZ_VERSION}" -H "x-ms-blob-type: BlockBlob" --data-binary "@network.log.har.gz" "${AZ_BLOB_TARGET}${TRAVIS_JOB_NUMBER}.har.gz?${AZ_SAS_TOKEN}"
+curl -v -X PUT -H "Content-Type: application/octet-stream" -H "x-ms-date: ${DATE_NOW}" -H "x-ms-version: ${AZ_VERSION}" -H "x-ms-blob-type: BlockBlob" --data-binary "@network.log" "${AZ_BLOB_TARGET}${TRAVIS_JOB_NUMBER}.network.log?${AZ_SAS_TOKEN}"
