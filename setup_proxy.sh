10894bbae2e9c8024ff9c936ac6d5cf62bcfc83f
Itai Bar-Haim <itaibh@gmail.com>
2021-03-06 12:30:00 +0200

    wip

--- setup_proxy.sh
+++ setup_proxy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 sudo apt-get install mitmproxy
-pwd
-ls -la
+mitmdump &
+curl --proxy "http://127.0.0.1:8080" https://mitm.it
+pkill mitmdump
 sudo mkdir /usr/share/ca-certificates/extra
 sudo cp ~/.mitmproxy/mitmproxy-ca-cert.cer /usr/share/ca-certificates/extra
 sudo echo "extra/mitmproxy-ca-cert.cer" >> /etc/ca-certificates.conf
