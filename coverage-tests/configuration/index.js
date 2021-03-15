4e2673a12f2020b66fa70c1c05897d4b5a289bc5
YuriyCheTest <yurii.cherepovskiy@applitools.com>
2021-03-15 11:08:12 +0200

    set link to overrides to test branch

--- coverage-tests/configuration/index.js
+++ coverage-tests/configuration/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'eyes_selenium_dotnet',
   emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/DotNet/emitter.js',
-  overrides: '../../../../../overrides.js',
+  overrides: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/remove_skip_for_DotNet2/DotNet/overrides.js',
   template: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/DotNet/template.hbs',
   ext: '.cs',
   outPath: './test/Selenium/coverage/generic'
