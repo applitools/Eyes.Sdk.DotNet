d5c07c84d4eb0ac08dc426890a299d6fbcbd086b
YuriyCheTest <yurii.cherepovskiy@applitools.com>
2021-03-17 14:49:20 +0200

    Set emitter, overrides links to test branch

--- coverage-tests/configuration/index.js
+++ coverage-tests/configuration/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'eyes_selenium_dotnet',
-  emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/DotNet/emitter.js',
-  overrides: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/DotNet/overrides.js',
+  emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/DotNet_add_layoutBreakpoints/DotNet/emitter.js',
+  overrides: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/DotNet_add_layoutBreakpoints/DotNet/overrides.js',
   template: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/DotNet/template.hbs',
   ext: '.cs',
   outPath: './test/Selenium/coverage/generic'
