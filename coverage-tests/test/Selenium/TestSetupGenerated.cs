945dd477960058ef96bef167f3554d3ab71e6a96
Itai Bar-Haim <itai.barhaim@applitools.com>
2021-03-18 21:18:05 +0200

    split tests base class to filtering and reporting. (#71)

--- coverage-tests/test/Selenium/TestSetupGenerated.cs
+++ coverage-tests/test/Selenium/TestSetupGenerated.cs
@@ -17,7 +17,7 @@ using System.Threading;
 
 namespace Applitools.Generated.Selenium.Tests
 {
-    public abstract class TestSetupGenerated// : ReportingTestSuiteGenerrated
+    public abstract class TestSetupGenerated : FilteringTestSuite
     {
 
         protected IWebDriver driver;
