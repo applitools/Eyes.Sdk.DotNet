240dff754495bdc86a9e52284791314cbe7e0c89
Itai Bar-Haim <itai.barhaim@applitools.com>
2021-04-08 11:38:41 +0300

    adding support for check window after manually switch to frame.

--- Eyes.Selenium.DotNet/SeleniumEyes.cs
+++ Eyes.Selenium.DotNet/SeleniumEyes.cs
@@ -1296,7 +1296,26 @@ namespace Applitools.Selenium
             {
                 jsExecutor_.ExecuteScript(SET_DATA_APPLITOOLS_SCROLL_ATTR, scrolledElement);
             }
+
+            Frame frame;
+            FrameChain frameChain = driver_.GetFrameChain();
+            FrameChain originalFrameChain = frameChain.Clone();
+            while ((frame = frameChain.Peek()) != null)
+            {
+                Rectangle frameBounds = new Rectangle(frame.Location, frame.InnerSize);
+                if (!effectiveViewport_.Contains(frameBounds))
+                {
+                    driver_.SwitchTo().ParentFrame();
+                    frameBounds = BringRegionToView(frameBounds, effectiveViewport_.Location);
+                    effectiveViewport_.Intersect(frameBounds);
+                }
+            }
             EyesWebDriverScreenshot result = GetScaledAndCroppedScreenshot_(scaleProviderFactory);
+            if (effectiveViewport_.Width < result.Image.Width || effectiveViewport_.Height < result.Image.Height)
+            {
+                result = (EyesWebDriverScreenshot)result.GetSubScreenshot(effectiveViewport_, false);
+            }
+            ((EyesWebDriverTargetLocator)driver_.SwitchTo()).Frames(originalFrameChain);
             return result;
         }
 
