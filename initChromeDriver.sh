223605bb854d74195033f644b37d4eac77f970e9
Itai Bar-Haim <itai.barhaim@applitools.com>
2021-04-14 11:25:53 +0300

    trying to get the correct version of chrome driver

--- initChromeDriver.sh
+++ initChromeDriver.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 chromeVersion=`google-chrome -- product-version | sed 's/\..*//'`
+echo $schromeVersion
 latestChromeDriverURL=$(wget http://chromedriver.storage.googleapis.com/LATEST_RELEASE_$chromeVersion -q -O -)
+echo $latestChromeDriverURL
 wget "http://chromedriver.storage.googleapis.com/${latestChromeDriverURL}/chromedriver_linux64.zip"
 unzip chromedriver_linux64.zip -d /home/travis/build/
 export CHROME_BIN=chromium-browser
