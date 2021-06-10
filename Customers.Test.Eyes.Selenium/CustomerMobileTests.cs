bdb9192668a391bc18065cbf19749b68ee954bbb
itai.barhaim <itai.barhaim@applitools.com>
2021-06-10 10:15:05 +0000

    added tests - currently only run manually.

--- Customers.Test.Eyes.Selenium/CustomerMobileTests.cs
+++ Customers.Test.Eyes.Selenium/CustomerMobileTests.cs
@@ -1,6 +1,9 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.iOS;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
@@ -35,6 +38,79 @@ namespace Applitools.Selenium.Tests
 
 
                 eyes.Close();
+            }
+            finally
+            {
+                eyes.AbortIfNotClosed();
+                driver.Quit();
+            }
+        }
+
+        [Test]
+        public void TestIOSFeaturesUsingRemoteWebDriver()
+        {
+            // This is your api key, make sure you use it in all your tests.
+            Eyes eyes = new Eyes();
+
+            // Setup appium - Ensure the capabilities meets your environment.
+            // Refer to http://appium.io documentation if required.
+            DesiredCapabilities dc = new DesiredCapabilities();
+            dc.SetCapability("platformName", "iOS");
+            dc.SetCapability("browserName", "Safari");
+            dc.SetCapability("platformVersion", "14");
+            dc.SetCapability("deviceName", "iPad Pro 12.9 2020");
+
+            dc.SetCapability("browserstack.user", TestDataProvider.BROWSERSTACK_USERNAME);
+            dc.SetCapability("browserstack.key", TestDataProvider.BROWSERSTACK_ACCESS_KEY);
+
+            IWebDriver driver = new RemoteWebDriver(new Uri(TestDataProvider.BROWSERSTACK_SELENIUM_URL), dc);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+
+
+            try
+            {
+                driver.Url = "https://www.lampsplus.com/products/wall-lights";
+
+                // Start the test.
+                eyes.Open(driver, "Lampsplus Ticket", "37479_2");
+                eyes.CheckWindow("ScreenShot", true);
+                eyes.Close(false);
+            }
+            finally
+            {
+                eyes.AbortIfNotClosed();
+                driver.Quit();
+            }
+        }
+
+        [Test]
+        public void TestIOSFeaturesUsingIOSDriver()
+        {
+            // This is your api key, make sure you use it in all your tests.
+            Eyes eyes = new Eyes();
+
+            // Setup appium - Ensure the capabilities meets your environment.
+            // Refer to http://appium.io documentation if required.
+            AppiumOptions options = new AppiumOptions();
+            options.AddAdditionalCapability("platformName", "iOS");
+            options.AddAdditionalCapability("browserName", "Safari");
+            options.AddAdditionalCapability("platformVersion", "14");
+            options.AddAdditionalCapability("deviceName", "iPad Pro 12.9 2020");
+            options.AddAdditionalCapability("browserstack.user", TestDataProvider.BROWSERSTACK_USERNAME);
+            options.AddAdditionalCapability("browserstack.key", TestDataProvider.BROWSERSTACK_ACCESS_KEY);
+
+            IWebDriver driver = new IOSDriver<AppiumWebElement>(new Uri(TestDataProvider.BROWSERSTACK_SELENIUM_URL), options);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+
+
+            try
+            {
+                driver.Url = "https://www.lampsplus.com/products/wall-lights";
+
+                // Start the test.
+                eyes.Open(driver, "Lampsplus Ticket", "37479_2");
+                eyes.CheckWindow("ScreenShot", true);
+                eyes.Close(false);
             }
             finally
             {
